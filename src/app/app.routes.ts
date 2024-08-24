32b58ef5372a5c22ae0317c8f3ec753352985b26
moosa <musa.saadatyar@gmail.com>
2024-08-24 09:07:34 +0330

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path:'login',
+
+  // },
+  // {
+  //   path:'',
+  // },
+  {
+    path:'**',
+    loadComponent:()=>import('./core/components/pages/forbidden403/forbidden403.component').then(m =>m.Forbidden403Component)
+  },
+  // {
+  //   path:'**'
+  // }
+];
+
+
+
+
