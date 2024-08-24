41ad407f7b53fb6751699fa2de9d4d8dc994d7d9
moosa <musa.saadatyar@gmail.com>
2024-08-24 09:07:22 +0330

    add forbidden page

--- src/app/core/components/pages/forbidden403/forbidden403.component.ts
+++ src/app/core/components/pages/forbidden403/forbidden403.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forbidden403',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './forbidden403.component.html',
-  styleUrl: './forbidden403.component.css'
+  styleUrl: './forbidden403.component.css',
 })
 export class Forbidden403Component {
-
   constructor(private router: Router) {}
+  private transLate = inject(TranslateService);
 
+  title = this.transLate.instant('UN_ACCESS');
   goBack() {
     window.history.back();
   }
