e72c7a1fa43dea541bb109fd033448b761d11efb
moosa <musa.saadatyar@gmail.com>
2024-09-11 17:51:43 +0330

    add header and user dropDown

--- src/app/core/components/static/header/header.component.ts
+++ src/app/core/components/static/header/header.component.ts
@@ -1,14 +1,46 @@
-import { Component} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone: true,
+  imports: [
+    AvatarModule, 
+    AvatarGroupModule, 
+    TranslateModule, 
+    RouterModule,
+    FormsModule,
+    NgbModule
+  ],
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
   currentCulture: string = 'EN';
+  profileUrl: string;
 
-  
+  currentUser$ = true;
+
+  userOption !: any[];
+
+  private translate=inject(TranslateService)
+  ngOnInit(): void {
+    this.profileUrl = `/kjsdf`;
+    this.userOption = [
+      {name: this.translate.instant('USER_ACCOUNT'), code:'User'}
+    ]
+  }
+
+  public redirectToResetPassword() {}
+
+  public logout() {
+    localStorage.clear();
+    sessionStorage.clear();
+    localStorage.clear();
+  }
 }
