350869b28090345e1e061f4da2ad03747ce17851
moosa <musa.saadatyar@gmail.com>
2024-09-11 17:53:37 +0330

    add nave header

--- src/app/core/components/static/nav-header/nav-header.component.ts
+++ src/app/core/components/static/nav-header/nav-header.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule, NgIf } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { CommonModule, isPlatformBrowser, NgIf } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { BehaviorSubject } from 'rxjs';
@@ -11,13 +11,7 @@ import { Unsubscriber } from '../../../utils/unsubscriber';
   templateUrl: './nav-header.component.html',
   styleUrl: './nav-header.component.css',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink,
-    TranslateModule,
-    NgIf,
-
-  ],
+  imports: [CommonModule, RouterLink, TranslateModule, NgIf],
 })
 export class NavHeaderComponent extends Unsubscriber {
   public $hamburgerIcon = new BehaviorSubject(true);
@@ -28,16 +22,22 @@ export class NavHeaderComponent extends Unsubscriber {
 
   private screenWidth: number;
 
-  constructor(public _sharedService: SharedService) {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    public _sharedService: SharedService
+  ) {
     super();
-    this.getScreenSize();
+    if (isPlatformBrowser(this.platformId)) {
+      this.getScreenSize();
+    }
   }
 
-  public getHamburgerIcon(){
-    this.anotherSubscription =
-      this._sharedService.$hamburgerClass.subscribe(val => {
+  public getHamburgerIcon() {
+    this.anotherSubscription = this._sharedService.$hamburgerClass.subscribe(
+      (val) => {
         this.$hamburgerIcon.next(val);
-      });
+      }
+    );
   }
 
   public toggleHamburgerClass() {
@@ -46,17 +46,19 @@ export class NavHeaderComponent extends Unsubscriber {
   }
 
   @HostListener('window:resize', ['$event'])
-  getScreenSize(event?:any) {
-    this.screenHeight = window.innerHeight;
-    this.screenWidth = window.innerWidth;
-    if (this.screenWidth < 768) {
-      this.windows = false;
-      document.body.setAttribute('data-sidebar-style', 'overlay');
-    } else if (this.screenWidth >= 768 && this.screenWidth <= 1023) {
-      document.body.setAttribute('data-sidebar-style', 'mini');
-      this.windows = false;
-    } else {
-      document.body.setAttribute('data-sidebar-style', 'full');
+  getScreenSize(event?: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.screenHeight = window.innerHeight;
+      this.screenWidth = window.innerWidth;
+      if (this.screenWidth < 768) {
+        this.windows = false;
+        document.body.setAttribute('data-sidebar-style', 'overlay');
+      } else if (this.screenWidth >= 768 && this.screenWidth <= 1023) {
+        document.body.setAttribute('data-sidebar-style', 'mini');
+        this.windows = false;
+      } else {
+        document.body.setAttribute('data-sidebar-style', 'full');
+      }
     }
   }
 }
